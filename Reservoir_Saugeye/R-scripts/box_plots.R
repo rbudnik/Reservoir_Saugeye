cd158e15c6180d844b859ddb808e344369f54f77
Rich Budnik <63796331+rbudnik@users.noreply.github.com>
2020-10-02 11:38:32 -0400

    Add files via upload

--- Reservoir_Saugeye/R-scripts/box_plots.R
+++ Reservoir_Saugeye/R-scripts/box_plots.R
@@ -1,5 +1,5 @@
 # Set working directory
-setwd("C:/Users/richa/OneDrive/Desktop/Reservoir_Saugeye")
+setwd("C:/Users/richa/OneDrive/Desktop/SAE_Retrospective/SAE_Retrospective")
 
 # Load data
 dat=read.csv("csv/Data_complete4.csv")
@@ -13,6 +13,7 @@ loadfonts(device = "win")
 
 # Response variable
 age0_cpue = dat$age0_cpue
+growth = dat$growth
 
 # Fixed effects 
 lbass_cpue = dat$lbass_cpue
@@ -27,6 +28,26 @@ size_ha = dat$size_ha
 # Possible random effects 
 location = as.factor(dat$location)
 year = as.factor(dat$year)
+
+
+############################ Box Plot Age0_cpue ####################################
+growth_plot <- ggplot(dat, aes(x=location, y=growth)) + 
+                  geom_boxplot() +
+                  ylab("Saugeye Growth") +
+                  theme(axis.text.y = element_text(colour = "black", 
+                                                   size = 12,
+                                                   family = "Times New Roman"),
+                        axis.title.y = element_text(colour = "black",
+                                                    size = 12,
+                                                    family = "Times New Roman"),
+                        axis.text.x = element_blank(),
+                        axis.title.x=element_blank(),
+                        panel.background = element_blank(), 
+                        panel.border = element_rect(colour = "black", 
+                                                    fill = NA, 
+                                                    size = 1.2),
+                        plot.margin = unit(c(0,0,0.15,0.75), "cm")) #top, right, bottom left
+growth_plot
 
 
 ############################ Box Plot Age0_cpue ####################################
@@ -74,16 +95,20 @@ lcrap_cpue_plot <- ggplot(dat, aes(x=location, y=lcrap_cpue)) +
                   theme(axis.text.y = element_text(colour = "black", 
                                                    size = 12,
                                                    family = "Times New Roman"),
+                        axis.text.x = element_text(colour = "black", 
+                                                   size = 12,
+                                                   family = "Times New Roman",
+                                                   angle = 55, 
+                                                   hjust = 1),
                         axis.title.y = element_text(colour = "black",
                                                     size = 12,
                                                     family = "Times New Roman"),
-                        axis.text.x = element_blank(),
                         axis.title.x=element_blank(),
                         panel.background = element_blank(), 
                         panel.border = element_rect(colour = "black", 
                                                     fill = NA, 
                                                     size = 1.2),
-                        plot.margin = unit(c(0.15,0,0.15,0.75), "cm")) #top, right, bottom left
+                        plot.margin = unit(c(0.15,0,0,0.75), "cm")) #top, right, bottom left
 lcrap_cpue_plot
 
 ############################ Box Plot Large adult saugeye CPUE ####################################
@@ -96,7 +121,7 @@ lsaug_cpue_plot <- ggplot(dat, aes(x=location, y=lsaug_cpue)) +
                         axis.text.x = element_text(colour = "black", 
                                                    size = 12,
                                                    family = "Times New Roman",
-                                                   angle = 45, 
+                                                   angle = 55, 
                                                    hjust = 1),
                         axis.title.y = element_text(colour = "black",
                                                     size = 12,
@@ -180,7 +205,7 @@ secchi_plot <- ggplot(dat, aes(x=location, y=secchi)) +
                         axis.text.x = element_text(colour = "black", 
                                                    size = 12,
                                                    family = "Times New Roman",
-                                                   angle = 45, 
+                                                   angle = 55, 
                                                    hjust = 1),
                         axis.title.x=element_blank(),
                         panel.background = element_blank(), 
@@ -192,24 +217,24 @@ secchi_plot <- ggplot(dat, aes(x=location, y=secchi)) +
 secchi_plot
 
 ##################### Combine Plots ################################################
-png(filename = paste("Figure2.png", sep = ""),
-     width = 6.5, height = 7.25, units = "in", res = 600, restoreConsole = TRUE)
+png(filename = paste("Figures/box_plots.png", sep = ""),
+     width = 6.53, height = 5.25, units = "in", res = 600, restoreConsole = TRUE)
 
-box_plots <- plot_grid(age0_cpue_plot, 
-                         sto_doy_plot,
-                         sto_dens_ha_plot, 
-                         sto_length_mm_plot, 
-                         lbass_cpue_plot,
-                         lcrap_cpue_plot, 
-                         lsaug_cpue_plot, 
-                         secchi_plot,
-                         nrow = 4,
-                         align = "v",
-                         labels = "AUTO",
-                         label_size = 14,
-                         label_fontfamily = "Times New Roman",
-                         rel_heights = c(1,1,1,1.43))
-
+box_plots <- plot_grid(growth_plot, 
+                       age0_cpue_plot, 
+                       sto_doy_plot, 
+                       sto_dens_ha_plot, 
+                       sto_length_mm_plot, 
+                       lbass_cpue_plot, 
+                       lcrap_cpue_plot,
+                       lsaug_cpue_plot,
+                       secchi_plot, 
+                       nrow = 3, 
+                       align = "v", 
+                       labels = "AUTO", 
+                       label_size = 14, 
+                       label_fontfamily = "Times New Roman", 
+                       rel_heights = c(1,1,1.43))
 
 
 box_plots
